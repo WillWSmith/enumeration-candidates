4351e597151a27698abfd66d3837a2f0676cd196
WillWSmith <will.w.smith94@gmail.com>
2024-05-04 12:45:24 -0400

    Add logic for filters.rb

--- filters.rb
+++ filters.rb
@@ -2,15 +2,33 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+    if @candidates.include?(id)
+      return id
+    else
+      return nil
+    end
   end
   
   def experienced?(candidate)
-    # Your code Here
+    if candidate.years_of_experience >= 2
+      return true
+    else
+      return false
+    end
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    for candidate in candidates
+      if experienced?(candidate) && (candidate.languages.include?('Ruby') || candidate.languages.include?('Python')) && candidate.github_points >= 100 && candidate.age >= 18 && (candidate.date_applied >= 15.days.ago.to_date)
+        return true
+      else
+        return false
+      end
+    end
   end
   
   # More methods will go below
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by {|candidate| [candidate[:years_of_experience], candidate[:github_points]]}.reverse
+  end
